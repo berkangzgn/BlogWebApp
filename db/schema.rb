1c05b6e1c9d9fc6c92cf3d41e8dc739954d73a3f
berkangzgn <bgerkanezgin.bg2014@gmail.com>
2020-09-21 15:12:26 +0300

    Show user article

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_09_18_081547) do
+ActiveRecord::Schema.define(version: 2020_09_21_115154) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -19,6 +19,8 @@ ActiveRecord::Schema.define(version: 2020_09_18_081547) do
     t.string "title"
     t.text "description"
     t.integer "user_id"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
   end
 
   create_table "users", force: :cascade do |t|
