7a00364208781bf85906e2f28b067b6d137176e0
berkangzgn <bgerkanezgin.bg2014@gmail.com>
2020-09-30 00:30:11 +0300

    Code was editing

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -11,7 +11,7 @@ class UsersController < ApplicationController
   def create
     @user = User.new(user_params)
     if @user.save
-      flash[:success] = "Welcome to the alpha blog #{@user.username}"
+      flash[:success] = "Welcome to the Word Life #{@user.username}"
       redirect_to articles_path
     else
       render :new; end
